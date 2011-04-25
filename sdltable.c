8a232062841eabe3aec907e31015cb1409d43533
Martin Stensgård <mastensg@ping.uio.no>
2011-04-25 05:54:49 +0200

    draw some text

--- sdltable.c
+++ sdltable.c
@@ -2,101 +2,76 @@
 #include <stdio.h>
 
 #include <SDL/SDL.h>
+#include <SDL/SDL_ttf.h>
 
-#define LENGTH(array)   (sizeof(array) / sizeof(array[0]))
+static const int sw = 1024;
+static const int sh = 768;
+static int running = 1;
 
-#if 0
-void
+static SDL_Surface *screen;
+static TTF_Font *font;
+
+static void
 handle_events(void) {
     SDL_Event event;
+
     while (SDL_PollEvent(&event)) {
         switch (event.type) {
-            case SDL_KEYDOWN:
-                if (event.key.keysym.sym == SDLK_ESCAPE)
-                    running = false;
-                lift = true;
-                break;
-            case SDL_KEYUP:
-                lift = false;
-                break;
-            default:
-                break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE)
+                running = 0;
+
+            break;
+        default:
+            break;
         }
     }
 }
 
-SDL_Surface *
-loadImage(const char *filename) {
-    SDL_Surface *loaded_image = IMG_Load(filename);
-    SDL_Surface *optimized_image;
+static void
+draw_table(int i) {
+    static const SDL_Color bg = {0, 0, 0, 255};
+    static const SDL_Color fg = {200, 200, 200, 255};
 
-    if (loaded_image)
-        optimized_image = SDL_DisplayFormatAlpha(loaded_image);
+    char msg[20];
+    sprintf(msg, "Now at %d.", i);
+    SDL_Surface *text = TTF_RenderText_Shaded(font, msg, fg, bg);
 
-    SDL_FreeSurface(loaded_image);
-    return optimized_image;
-}
+    SDL_FillRect(screen, &screen->clip_rect, SDL_MapRGB(screen->format, 0, i % 0x100, 0xff));
 
-void
-paint(void) {
-    int i;
-    SDL_Rect level_line = {0, 0};
+    static SDL_Rect pos = {30, 40};
+    SDL_BlitSurface(text, NULL, screen, &pos);
 
-    SDL_FillRect(screen, &screen->clip_rect, SDL_MapRGB(screen->format, 0, 0, 0xff));
-    for (i = level_elapsed; i < level_elapsed + window_width; ++i) {
-        ++level_line.x;
-        level_line.y = window_height - world[i];
-        level_line.w = 1;
-        level_line.h = world[i];
-        SDL_FillRect(screen, &level_line, SDL_MapRGB(screen->format, 0, 0xff, 0));
-    }
-    SDL_BlitSurface(bumblebee, NULL, screen, &bumblebee_position);
     SDL_Flip(screen);
 }
 
-void
-run(void) {
-    while (running) {
-        handle_events();
-        update_gamestate();
-        paint();
-    }
+static void
+font_init(void) {
+    if(TTF_Init() == -1)
+        err(1, "Could not initialize font library");
+
+    static const char *fontpath = "DejaVuSans.ttf";
+    static const int fontsize = 48;
+
+    font = TTF_OpenFont(fontpath, fontsize);
+    if(font == NULL)
+        err(1, "Could not load font \"%s\"", fontpath);
 }
-
-void
-update_gamestate(void) {
-    Uint32 t = SDL_GetTicks();
-    Uint32 dt = t - last_t;
-    float acceleration;
-
-    last_t = t;
-    if ((accum_dt += dt) < max_dt)
-        return;
-
-    accum_dt = 0;
-    acceleration = lift ? 1.3 : -1.7;
-    velocity += acceleration;
-    bumblebee_position.y -= 0.04 * velocity;
-
-    if (bumblebee_position.y > window_height - world[level_elapsed + bumblebee_position.x]) {
-        bumblebee_position.y = window_height / 2;
-        velocity = 0;
-        level_elapsed = 0;
-    } else
-        ++level_elapsed;
-}
-#endif
 
 int
 main(int argc, char *argv[]) {
-    static const int sw = 100;
-    static const int sh = 100;
+    font_init();
 
-    SDL_Surface *screen = SDL_SetVideoMode(sw, sh, 0, SDL_SWSURFACE | SDL_RESIZABLE);
+    screen = SDL_SetVideoMode(sw, sh, 0, SDL_SWSURFACE | SDL_RESIZABLE);
     if(!screen)
         err(1, "Could not initialize screen");
 
-    for(;;);
+    int i = 0;
+    while(running) {
+        handle_events();
+        draw_table(i);
+        ++i;
+    }
 
     return EXIT_SUCCESS;
 }
